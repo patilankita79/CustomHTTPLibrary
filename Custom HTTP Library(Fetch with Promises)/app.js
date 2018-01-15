da2c0b416982b6905ac8076f9dcd77bfdf453ca1
Ankita Patil <patilankita79@gmail.com>
2018-01-14 18:51:36 -0600

    Update app.js

--- Custom HTTP Library(Fetch with Promises)/app.js
+++ Custom HTTP Library(Fetch with Promises)/app.js
@@ -1,9 +1,11 @@
 const http = new EasyHTTP;
 
+// Please comment the other HTTP request methods while testing
+
 // Get Users
-// http.get('https://jsonplaceholder.typicode.com/users')
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+ http.get('https://jsonplaceholder.typicode.com/users')
+     .then(data => console.log(data))
+     .catch(err => console.log(err));
 
 
 // Create User data
@@ -13,15 +15,15 @@ const data = {
   email: 'johndoe@gmail.com'
 }
 
-// // Create a user
-// http.post('https://jsonplaceholder.typicode.com/users', data)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+ // Create a user
+ http.post('https://jsonplaceholder.typicode.com/users', data)
+     .then(data => console.log(data))
+   .catch(err => console.log(err));
 
 // Update a user
-// http.put('https://jsonplaceholder.typicode.com/users/2', data)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+ http.put('https://jsonplaceholder.typicode.com/users/2', data)
+     .then(data => console.log(data))
+     .catch(err => console.log(err));
 
 // Delete a user   
 http.delete('https://jsonplaceholder.typicode.com/users/2')
