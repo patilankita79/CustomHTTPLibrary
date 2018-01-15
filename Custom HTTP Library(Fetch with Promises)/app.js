15a3067edabb79df1c6413a9d6ef7395663744fd
Ankita Patil <patilankita79@gmail.com>
2018-01-14 18:47:17 -0600

    Using Fetch API with promises

---
+++ Custom HTTP Library(Fetch with Promises)/app.js
@@ -0,0 +1,29 @@
+const http = new EasyHTTP;
+
+// Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+
+// Create User data
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'johndoe@gmail.com'
+}
+
+// // Create a user
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// Update a user
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// Delete a user   
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
